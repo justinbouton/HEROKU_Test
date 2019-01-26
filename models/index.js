f0552caad492ff6403a1e12683d07afff88656d9
justbouton <justb1985@gmail.com>
2019-01-26 10:34:53 -0800

    index.js change disabled first var sequelize

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// var sequelize = new Sequelize(config.database, config.username, config.password, config);
 const db = {};
 
 
