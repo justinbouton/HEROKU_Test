b4545aa65815b6219d1eae98804c472c1b2bbb32
justbouton <justb1985@gmail.com>
2019-01-26 08:41:21 -0800

    index.js removed db or. config.json restored. env set to prod

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
-const db = process.env.DATABASE_URL || {};
+const db = {};
 
 fs
     .readdirSync(__dirname)
